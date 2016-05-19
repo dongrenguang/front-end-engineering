bd96bb68dad78c2b30418f24e141f4b8d3295250
Renguang Dong <dongrenguang@gmail.com>
2016-05-19 14:41:45 +0800

    Modify gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
 const gulp = require("gulp");
 const rimraf = require("gulp-rimraf");
+const runSequence = require("run-sequence");
+const babel = require("gulp-babel");
 
 const SRC_PATH = "./src";
 const DEST_PATH = "./public/assets";
 
-gulp.task("default");
+gulp.task("default", [ "build" ]);
 
-gulp.task("")
+gulp.task("clean", () => {
+    return gulp.src(DEST_PATH)
+        .pipe(rimraf());
+});
+
+gulp.task("build", [ "clean" ], (cb) => {
+    runSequence(
+        [ "build-js" ],
+        cb
+    );
+});
+
+gulp.task("build-js", () => {
+    return gulp.src(`${SRC_PATH}/**/*.js`)
+        .pipe(babel({
+            sourceRoot: SRC_PATH,
+            presets: [ "es2015" ]
+        }))
+        .pipe(gulp.dest(DEST_PATH));
+})
